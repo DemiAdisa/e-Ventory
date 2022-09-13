ae261a9fc2b652d72ce45ca031ee8d94373154b5
Demi Adisa <demi@reddingdesigns.com>
2022-09-13 11:02:02 -0300

    Login Page and Register PAge Created

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  late double _deviceWidth, _deviceHeight;
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: _deviceWidth * 0.05,
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                titleWidget(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget titleWidget() {
+    return const Text(
+      "e-Ventory",
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 25,
+        fontWeight: FontWeight.w600,
+      ),
+    );
+  }
+}
